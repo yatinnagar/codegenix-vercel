39d511384b2b142bd956c0b4fdc2c9fdfdb79c1a
Yatin <yatinnagar871@gmail.com>
2022-06-21 00:20:52 +0530

    changed read methods category and tag related blogs controllers

--- controllers/tag.js
+++ controllers/tag.js
@@ -1,4 +1,6 @@
 const Tag =require('../models/tag');
+const Blog =require('../models/blog');
+
 const slugify=require('slugify');
 const {errorHandler}=require('../helpers/dbErrorHandler')
 exports.create=(req,res)=>{
@@ -36,7 +38,21 @@ exports.create=(req,res)=>{
                     error:errorHandler(err)
                 })
             }
-            res.json(tag);
+            // res.json(tag);
+            Blog.find({tags:tag})
+            .populate('categories','_id name slug')
+            .populate('tags','_id name slug')
+            .populate('postedBy','_id name')
+            .select('_id title slug excerpt categories tags postedBy createdAt updatedAt')
+            .exec((err,data)=>{
+                if(err){
+                    return res.status(400).json({
+                        error:errorHandler(err)
+                    });
+                }
+                
+                res.json({tag,blogs:data})
+            })
         })
     }
     exports.remove=(req,res)=>{
